c78e31cb9c36c7cd07e398fcdac5bae3c7666b41
clfak <clfak.671@gmail.com>
2020-12-26 23:08:12 -0300

    techo

---
+++ Assets/Footsteps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+
+    public  AudioSource audioSource;
+
+    CharacterController controller;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        if (controller.isGrounded == true && controller.velocity.magnitude > 2f && audioSource.isPlaying == false)
+        {
+            audioSource.volume = Random.Range(0.8f, 1f);
+            audioSource.pitch = Random.Range(0.8f, 1.1f);
+            audioSource.Play();
+        }
+    }
+}
